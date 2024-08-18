b97ae491453cc3a0736e6c205b4fd50ef3e66888
haley227 <haleysmith227@gmail.com>
2024-08-17 21:47:47 -0700

    Update color constants

--- visa-app-client/src/consts/colors.ts
+++ visa-app-client/src/consts/colors.ts
@@ -1,8 +1,10 @@
 const COLORS = {
-    grey1: '#F5F5F5', 
+    black: '#000000',
+    grey1: '#f5f5f5',
     successGreen: '#4CAF50',
-    skyBlue: '#03A9F4',
-    errorRed: '#F44336',
+    skyBlue: '#54b3d6',
+    visaBlue: '#1A1F71',
+    errorRed: '#8B0000',
 } as const;
 
 export default COLORS;
