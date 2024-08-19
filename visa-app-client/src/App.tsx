904664355fefdfea69a58ccf3e931b289005790b
haley227 <haleysmith227@gmail.com>
2024-08-19 09:45:55 -0700

    Remove default app files

--- visa-app-client/src/App.tsx
+++ visa-app-client/src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import './App.css';
 import { BookList } from './components/pages/BookList';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from './components/pages/Layout';
