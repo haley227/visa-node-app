346889758022f3c62e36f743f93c698663a483ad
haley227 <haleysmith227@gmail.com>
2024-08-19 18:06:19 -0700

    Update ESLint Config

--- visa-app-client/src/.eslintrc.cjs
+++ visa-app-client/src/.eslintrc.cjs
@@ -1,9 +1,5 @@
 module.exports = {
   rules: {
-    'unicorn/prefer-includes': 'error',
-    'unicorn/prefer-keyboard-event-key': 'error',
-    'unicorn/prefer-date-now': 'error',
-    'unicorn/prefer-dom-node-append': 'error',
     '@typescript-eslint/no-non-null-assertion': 'off',
   }
 };
