98fb5405b9ce9c05b006742739e090ed69a9a00a
haley227 <haleysmith227@gmail.com>
2024-08-19 17:53:41 -0700

    Add prettier, jest typescript setting, and eslint settings

---
+++ visa-app-client/src/.eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  rules: {
+    'unicorn/prefer-includes': 'error',
+    'unicorn/prefer-keyboard-event-key': 'error',
+    'unicorn/prefer-date-now': 'error',
+    'unicorn/prefer-dom-node-append': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  }
+};
