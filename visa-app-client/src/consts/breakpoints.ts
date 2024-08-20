98126f06d8586320230c63067dc47ce4410c8704
haley227 <haleysmith227@gmail.com>
2024-08-19 21:08:21 -0700

    Add breakpoint for large mobile devices

--- visa-app-client/src/consts/breakpoints.ts
+++ visa-app-client/src/consts/breakpoints.ts
@@ -1,10 +1,17 @@
 import { css, StyledObject } from 'styled-components';
 
 const size = {
+  mobile: '400px',
   tablet: '768px',
   desktop: '1024px',
   desktopL: '1440px',
 };
+
+export const mobileUp = (inner: any) => css`
+  @media (min-width: ${size.mobile}) {
+    ${inner};
+  }
+`;
 
 export const tabletUp = (inner: any) => css`
   @media (min-width: ${size.tablet}) {
